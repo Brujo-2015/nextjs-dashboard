784ab446c8745ea381873529d73f4f4bf2f374ea
Hugo Villalva <hugo.villalva@gmail.com>
2026-09-10 13:42:20 -0600

    Initial commit

--- src/app/ui/invoices/status.jsx
+++ src/app/ui/invoices/status.jsx
@@ -1,4 +1,4 @@
-import { CheckIcon, ClockIcon } from '@heroicons/react/24/outline';
+import { CheckIcon, ClockIcon,   DocumentIcon, ExclamationTriangleIcon, } from '@heroicons/react/24/outline';
 import clsx from 'clsx';
 
 export default function InvoiceStatus({ status }) {
@@ -7,11 +7,20 @@ export default function InvoiceStatus({ status }) {
       className={clsx(
         'inline-flex items-center rounded-full px-2 py-1 text-xs',
         {
+          'bg-blue-100 text-blue-700': status === 'draft',
           'bg-gray-100 text-gray-500': status === 'pending',
           'bg-green-500 text-white': status === 'paid',
+          'bg-red-500  text-white': status === 'late',
+          'bg-red-700  text-white': status === 'overdue',
         },
       )}
     >
+      {status === 'draft' ? (
+        <>
+          Draft
+          <DocumentIcon className="ml-1 w-4 text-blue-700" />
+        </>
+      ) : null}
       {status === 'pending' ? (
         <>
           Pending
@@ -22,6 +31,18 @@ export default function InvoiceStatus({ status }) {
         <>
           Paid
           <CheckIcon className="ml-1 w-4 text-white" />
+        </>
+      ) : null}
+      {status === 'late' ? (
+        <>
+          Late
+          <ClockIcon className="ml-1 w-4 text-white" />
+        </>
+      ) : null}
+      {status === 'overdue' ? (
+        <>
+          Overdue
+          <ExclamationTriangleIcon className="ml-1 w-4 text-white" />
         </>
       ) : null}
     </span>
